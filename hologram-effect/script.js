993c08e60a6f52b0b04d64fe6b0b4f943b12fbe1
rizwanc018 <rizwanc018@gmail.com>
2025-09-01 08:00:12 +0530

    anchor position example created

--- hologram-effect/script.js
+++ hologram-effect/script.js
@@ -8,6 +8,19 @@ const updateCursor = (e) => {
     
     card.style.setProperty("--x", x);
     card.style.setProperty("--y", y);
+    
+    const centerX = rect.width / 2;
+    const centerY = rect.height / 2;
+    
+    const rotateX = ((y - centerY) / centerY) * -20;
+    const rotateY = ((x - centerX) / centerX) * 20;
+    
+    container.style.transform = `rotateX(${rotateX}deg) rotateY(${rotateY}deg)`;
 };
 
-card.addEventListener("pointermove", updateCursor);
+const resetCard = () => {
+    container.style.transform = `rotateX(0deg) rotateY(0deg)`;
+};
+
+container.addEventListener("pointermove", updateCursor);
+container.addEventListener("mouseleave", resetCard);
